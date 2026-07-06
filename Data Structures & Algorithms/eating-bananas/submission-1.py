56e676c8bb519cf5e7b035819ebd8cb85f61d99d
amana77 <amana.forms@gmail.com>
2026-07-06 22:14:27 +0530

    Add: eating-bananas - submission-1

---
+++ Data Structures & Algorithms/eating-bananas/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        res=max(piles)
+        l,r=1,res
+        while l<=r:
+            k=(l+r)//2
+            e=0
+            for b in piles:
+                e-=(-b//k)
+            if e<=h:
+                r=k-1
+                res=min(k,res)
+            elif e>h:
+                l=k+1
+        return res
+
+
+            
